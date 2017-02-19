c9942c7906662cbdbec3a4e9535a09a74400eaef
Lawrence Luo <lluo19@cranbrook.edu>
2017-02-19 11:31:55 -0500

    Update ServoControl

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoControl.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.hardware.Servo;
 import static org.firstinspires.ftc.teamcode.ComponentsInit.ComponentMap.*;
 
 public class ServoControl {
+    //TODO rename servos
 
     public ServoControl(Servo...servos){
         this.servos=servos;
@@ -18,5 +19,41 @@ public class ServoControl {
         servos[S_TOP_LEFT].setPosition(0.2);
         servos[S_TOP_RIGHT].setPosition(0.2);
     }
+
+    public void intakeForward(){
+        servos[INTAKE_1].setPosition(1);
+        servos[INTAKE_2].setPosition(0);
+        servos[INTAKE_3].setPosition(1);
+    }
+
+    public void intakeReverse(){
+        servos[INTAKE_1].setPosition(0);
+        servos[INTAKE_2].setPosition(1);
+        servos[INTAKE_3].setPosition(0);
+    }
+
+    public void intakeStop(){
+
+        servos[INTAKE_1].setPosition(0.5);
+        servos[INTAKE_2].setPosition(0.5);
+        servos[INTAKE_3].setPosition(0.5);
+    }
+
+    public void feedForward(){
+        servos[FEED_1].setPosition(1);
+        servos[FEED_2].setPosition(1);
+    }
+
+    public void feedReverse(){
+        servos[FEED_1].setPosition(1);
+        servos[FEED_2].setPosition(1);
+    }
+
+    public void feedStop(){
+        servos[FEED_1].setPosition(1);
+        servos[FEED_2].setPosition(1);
+    }
+
+
     Servo[] servos;
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOPMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOPMode.java
@@ -7,28 +7,30 @@ import org.firstinspires.ftc.robotcore.external.Func;
 
 import static org.firstinspires.ftc.teamcode.ComponentsInit.ComponentMap.M_FRONT_LEFT;
 
-@TeleOp(name="TeleOPMode")
-        public class TeleOPMode extends ComponentsInit {
-            public void init(){
-                super.sensorInit();
-                super.MotorInit();
-                //super.liftMortorInit();
-                //liftControl=new LiftControl(super.getMotors());
-                hDrive=new HolonomicDrive(super.getMotors());
-                //flyWheel=new FlyWheel(super.getMotors());
-                //intake=new Intake(super.getCRServos());
-                //tDrive=new TankDrive(super.getMotors());
-                //servoControl=new ServoControl(super.getServos());
+@TeleOp(name = "TeleOPMode")
+public class TeleOPMode extends ComponentsInit {
+    public void init() {
+        super.sensorInit();
+        super.MotorInit();
+        //super.liftMortorInit();
+        //liftControl=new LiftControl(super.getMotors());
+        hDrive = new HolonomicDrive(super.getMotors());
+        //flyWheel=new FlyWheel(super.getMotors());
+        //intake=new Intake(super.getCRServos());
+        //tDrive=new TankDrive(super.getMotors());
+        //servoControl=new ServoControl(super.getServos());
 
-                //sAccel.composeTelemetry(telemetry);
-            }
-            public void loop(){
-                hDrive.setValues(super.driveX(),super.driveLY(), super.driveR());
-                //telemetry.addData("position ", hDrive.encoderValues(M_FRONT_LEFT));
-                //sAccel.calcAccel();
-                //telemetry.addData("position:", sAccel.positions.x+""+sAccel.positions.y+""+sAccel.positions.z+"");
+        //sAccel.composeTelemetry(telemetry);
+    }
 
-                    }
+    public void loop() {
+        hDrive.setValues(super.driveX(), super.driveLY(), super.driveR());
+        //telemetry.addData("position ", hDrive.encoderValues(M_FRONT_LEFT));
+        //sAccel.calcAccel();
+        //telemetry.addData("position:", sAccel.positions.x+""+sAccel.positions.y+""+sAccel.positions.z+"");
+
+    }
+
     Intake intake;
     FlyWheel flyWheel;
     LiftControl liftControl;
